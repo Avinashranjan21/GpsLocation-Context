cf05382dd64cd69442b68effd144220d6c91951e
Avinash Ranjan <Avinash.ranjan21@gmail.com>
2015-09-03 02:06:14 +0530

    Location services call has been added to th mainactivity text pending.

--- app/src/main/java/com/googlePlayService/Location/MainActivity.java
+++ app/src/main/java/com/googlePlayService/Location/MainActivity.java
@@ -1,21 +1,41 @@
 package com.googlePlayService.Location;
 
+import android.location.Location;
 import android.location.LocationListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity  implements GoogleApiClient.ConnectionCallbacks ,
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener{
 
+    private final String LOG_TAG ="AvinashTestApp";
+    private TextView txtOutput;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoogleApiClient=new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        txtOutput = (TextView) findViewById(R.id.txtOutput);
+
     }
 
     @Override
@@ -38,5 +58,57 @@ public class MainActivity extends AppCompatActivity  implements GoogleApiClient.
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect(); // connect the google api client when onstart is called
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect(); // disconnect the google api client when onstop is called
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,mLocationRequest, (com.google.android.gms.location.LocationListener) this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(LOG_TAG,"Google API Client suspended the connection ");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(LOG_TAG,"Google API Client Failed to connect ");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i(LOG_TAG,location.toString());
+        txtOutput.setText(location.toString());
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
